c51633aa826c55487014cea8b6075da0ed64196c
William Gadd <wgadd@sasktel.net>
2022-11-10 21:25:55 -0600

    Use helper functions already made to display the relevant data from the server to the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,27 +4,34 @@ import axios from "axios";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
     value: "1pm",
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday",
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3",
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -70,7 +77,6 @@ class Dashboard extends Component {
   }
 
   render() {
-    console.log(this.state);
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
     });
@@ -78,10 +84,9 @@ class Dashboard extends Component {
     .map(panel => (
     < Panel
       key={panel.id}
-      id={panel.id}
       label={panel.label}
-      value={panel.value}
-      onSelect={event => this.selectPanel(panel.id)}
+      value={panel.getValue(this.state)}
+      onSelect={() => this.selectPanel(panel.id)}
     />
     ));
     
